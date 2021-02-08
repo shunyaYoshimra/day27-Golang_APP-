cb4513aa6ece407b68bbfaaceae869174be6becf
shunyaYoshimura <maoorgri1015@gmail.com>
2021-02-08 12:39:16 +0900

    created question repository, controller

---
+++ backend/controllers/question_controller.go
@@ -0,0 +1,126 @@
+package controllers
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/shunyaYoshimra/day27/backend/database/entity"
+
+	"github.com/shunyaYoshimra/day27/backend/middleware"
+
+	"github.com/shunyaYoshimra/day27/backend/utils/response"
+
+	"github.com/gin-gonic/gin"
+	"github.com/shunyaYoshimra/day27/backend/repositories"
+)
+
+type QuestionController struct {
+	Repository repositories.QuestionRepository
+}
+
+func NewQuestionController() QuestionController {
+	return QuestionController{
+		Repository: repositories.NewQuestionRepository(),
+	}
+}
+
+func (qc *QuestionController) Index(c *gin.Context) {
+	questions := qc.Repository.RetrieveQuestions()
+	c.JSON(http.StatusOK, questions)
+}
+
+func (qc *QuestionController) SearchedIndex(c *gin.Context) {
+	about := c.Param("about")
+	questions := qc.Repository.FindByAbout(about)
+	c.JSON(http.StatusOK, questions)
+}
+
+func (qc *QuestionController) UserQuestions(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("user-id"))
+	questions := qc.Repository.FindByUser(id)
+	c.JSON(http.StatusOK, questions)
+}
+
+func (qc *QuestionController) Show(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+	if question, err := qc.Repository.FindByID(id); err != nil {
+		res := response.NotFound("質問が見つかりませんでした(The question was not found)")
+		c.JSON(res.Status, res)
+	} else {
+		res := response.SuccessResponse("")
+		res.Data = question
+		c.JSON(res.Status, res)
+	}
+}
+
+func (qc *QuestionController) Create(c *gin.Context) {
+	title := c.PostForm("title")
+	content := c.PostForm("contetnt")
+	about := c.PostForm("about")
+	userID := middleware.GetSession(c)
+	if title == "" || content == "" || about == "" {
+		res := response.BadRequest("全ての情報を入力して下さい(All information should be filled in)")
+		c.JSON(res.Status, res)
+	} else {
+		question := entity.Question{
+			Title:   title,
+			Content: content,
+			About:   about,
+			UserID:  userID,
+		}
+		if err := qc.Repository.Create(&question); err != nil {
+			res := response.BadRequest("予期せぬエラーが発生しました(An unexpected error has occured)")
+			c.JSON(res.Status, res)
+		} else {
+			res := response.SuccessResponse("")
+			res.Data = question
+			c.JSON(res.Status, res)
+		}
+	}
+}
+
+func (qc *QuestionController) Update(c *gin.Context) {
+	title := c.PostForm("title")
+	content := c.PostForm("content")
+	id, _ := strconv.Atoi(c.Param("id"))
+	if question, err := qc.Repository.FindByID(id); err != nil {
+		res := response.NotFound("質問が見つかりませんでした(This question was not found)")
+		c.JSON(res.Status, res)
+	} else if err := qc.Repository.Update(question, title, content); err != nil {
+		res := response.BadRequest("予期せぬエラーが発生しました(An unexpected error has occured)")
+		c.JSON(res.Status, res)
+	} else {
+		res := response.SuccessResponse("")
+		res.Data = question
+		c.JSON(res.Status, res)
+	}
+}
+
+func (qc *QuestionController) ChangeStatus(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+	if question, err := qc.Repository.FindByID(id); err != nil {
+		res := response.NotFound("質問が見つかりませんでした(This question was not found)")
+		c.JSON(res.Status, res)
+	} else if err := qc.Repository.ChangeStatus(question); err != nil {
+		res := response.BadRequest("予期せぬエラーが発生しました(An unexpected error has occured)")
+		c.JSON(res.Status, res)
+	} else {
+		res := response.SuccessResponse("")
+		res.Data = question
+		c.JSON(res.Status, res)
+	}
+}
+
+func (qc *QuestionController) Delete(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+	if question, err := qc.Repository.FindByID(id); err != nil {
+		res := response.NotFound("質問が見つかりませんでした(This question was not found)")
+		c.JSON(res.Status, res)
+	} else if err := qc.Repository.Delete(question); err != nil {
+		res := response.BadRequest("予期せぬエラーが発生しました(An unexpected error has occured)")
+		c.JSON(res.Status, res)
+	} else {
+		res := response.SuccessResponse("")
+		c.JSON(res.Status, res)
+	}
+}
---
+++ backend/repositories/question_repository.go
@@ -0,0 +1,55 @@
+package repositories
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/shunyaYoshimra/day27/backend/database"
+	"github.com/shunyaYoshimra/day27/backend/database/entity"
+)
+
+type QuestionRepository struct {
+	Conn *gorm.DB
+}
+
+func NewQuestionRepository() QuestionRepository {
+	return QuestionRepository{Conn: database.GetDB().Table("questions")}
+}
+
+func (qr *QuestionRepository) RetrieveQuestions() (questions []entity.Question) {
+	qr.Conn.Order("created_at desc").Find(&questions)
+	return
+}
+
+func (qr *QuestionRepository) FindByID(id int) (question entity.Question, err error) {
+	err = qr.Conn.First(&question, id).Error
+	return
+}
+
+func (qr *QuestionRepository) FindByUser(id int) (questions []entity.Question) {
+	qr.Conn.Where("user_id = ?", id).Order("created_at desc").Find(&questions)
+	return
+}
+
+func (qr *QuestionRepository) FindByAbout(about string) (questions []entity.Question) {
+	qr.Conn.Where("about = ?", about).Order("created_at desc").Find(&questions)
+	return
+}
+
+func (qr *QuestionRepository) Create(question *entity.Question) (err error) {
+	err = qr.Conn.Create(question).Error
+	return
+}
+
+func (qr *QuestionRepository) Update(question entity.Question, title, content string) (err error) {
+	err = qr.Conn.Model(&question).Update(entity.Question{Title: title, Content: content}).Error
+	return
+}
+
+func (qr *QuestionRepository) ChangeStatus(question entity.Question) (err error) {
+	err = qr.Conn.Model(&question).Update(entity.Question{Status: false}).Error
+	return
+}
+
+func (qr *QuestionRepository) Delete(question entity.Question) (err error) {
+	err = qr.Conn.Delete(&question).Error
+	return
+}
