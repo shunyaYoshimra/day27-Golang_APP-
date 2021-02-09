8466efd6d82a999108efeda3829f1bad37ea1ee7
shunyaYoshimura <maoorgri1015@gmail.com>
2021-02-09 13:36:58 +0900

    modify auto migration

--- backend/database/migration/auto_migration.go
+++ backend/database/migration/auto_migration.go
@@ -6,5 +6,5 @@ import (
 )
 
 func AutoMigration(conn *gorm.DB) {
-	conn.AutoMigrate(entity.User{}, entity.Profile{}, entity.Contact{}, entity.Abroad{}, entity.Occupation{}, entity.Question{})
+	conn.AutoMigrate(entity.User{}, entity.Profile{}, entity.Contact{}, entity.Abroad{}, entity.Occupation{}, entity.Question{}, entity.Answer{})
 }
